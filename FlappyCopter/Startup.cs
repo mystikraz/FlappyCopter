d4acb49f8ae88f722870dea72e038fb770ceb890
Rajan <rajan.tandukar@billidentity.com>
2022-01-08 17:40:38 +0545

    fix

--- FlappyCopter/Startup.cs
+++ FlappyCopter/Startup.cs
@@ -66,7 +66,9 @@ namespace FlappyCopter
             }
             else
             {
-                app.UseExceptionHandler("/Home/Error");
+                app.UseDeveloperExceptionPage();
+
+                //app.UseExceptionHandler("/Home/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
